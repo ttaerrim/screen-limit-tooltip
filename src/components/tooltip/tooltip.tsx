a18257e5df99d4b731f41bf8ec699e95e389895e
ttaerrim <ltr0121@naver.com>
2024-04-28 18:05:30 +0900

    feat: 툴팁이 화면을 넘지 않게 재배치

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -1,5 +1,6 @@
 import styles from './tooltip.module.css';
 import IconQna from '../../assets/icon_qna.svg';
+import { useEffect, useRef, useState } from 'react';
 
 interface Props {
   title?: string;
@@ -7,11 +8,30 @@ interface Props {
 }
 
 export const Tooltip = ({ message, title }: Props) => {
+  const [screenWidth, setScreenWidth] = useState(0);
+  const [mouseX, setMouseX] = useState(0);
+  const [x, setX] = useState(0);
+  const tooltipRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    setScreenWidth(window.innerWidth);
+  }, []);
+
+  useEffect(() => {
+    const tooltipWidth = tooltipRef.current?.clientWidth || 0;
+
+    if (mouseX + tooltipWidth > screenWidth) {
+      setX(mouseX + tooltipWidth - screenWidth);
+    } else {
+      setX(0);
+    }
+  }, [mouseX, screenWidth]);
+
   return (
-    <div className={styles.tooltip}>
+    <div className={styles.tooltip} onMouseOver={(e) => setMouseX(e.screenX)}>
       <img className={styles['icon']} src={IconQna} alt='qna' width={16} height={16} />
       <div className={styles['arrow']} />
-      <div className={styles['tooltip']}>
+      <div style={{ left: x > 0 ? `-${x}px` : '0' }} className={styles['tooltip']} ref={tooltipRef}>
         {title && <p className={styles['header']}>{title}</p>}
         {message.map((message, index) => (
           <p className={styles['body']} key={index}>
